2b5c536ef08e333a80662b09c8a89012f5259e56
Andy Gurden <as.gurden@gmail.com>
2013-01-02 01:27:25 +0000

    Better argument names for rule apply methods

--- src/engine/Rule.py
+++ src/engine/Rule.py
@@ -15,7 +15,7 @@ class Rule(EqualsMixin, metaclass=abc.ABCMeta):
         self._deduction_filters = deduction_filters
     
     @abc.abstractmethod
-    def apply(self, add, allfacts, alldeductions, *factsanddeductions):
+    def apply(self, add, allfacts, alldeductions, **factsanddeductions):
         '''Given a set of items, use the add method to add all generated deductions.'''
     
     def get_applier(self, custom_deduction_check=None):
@@ -25,11 +25,11 @@ class RuleApplier:
     '''Allows collection of relevant facts or deductions for later application.'''
     
     def __init__(self, factfilters, deductionfilters, apply, custom_deduction_check=None):
-        '''Takes a list of lamba expressions that accept facts or deductions and say whether they are relevant.'''
+        '''Takes a dict of lamba expressions that accept facts or deductions and say whether they are relevant.'''
         self._factfilters = factfilters
         self._deductionfilters = deductionfilters
-        self._relevant_facts = [set() for _ in self._factfilters]
-        self._relevant_deductions = [set() for _ in self._deductionfilters]
+        self._relevant_facts = {key: set() for key in self._factfilters}
+        self._relevant_deductions = {key: set() for key in self._deductionfilters}
         self._apply = apply
         self._custom_deduction_check = custom_deduction_check
     
@@ -40,9 +40,9 @@ class RuleApplier:
     
     def show_fact(self, fact):
         '''Show a fact, it will be recorded if it is relevant.'''
-        for i, test in enumerate(self._factfilters):
+        for key, test in self._factfilters.items():
             if test(fact):
-                self._relevant_facts[i].add(fact)
+                self._relevant_facts[key].add(fact)
     
     def show_deductions(self, deductions):
         for deduction in deductions:
@@ -50,24 +50,24 @@ class RuleApplier:
     
     def show_deduction(self, deduction):
         '''Show a deduction, it will be recorded if it is relevant.'''
-        for i, test in enumerate(self._deductionfilters):
+        for key, test in self._deductionfilters.items():
             if test(deduction):
-                self._relevant_deductions[i].add(deduction)
+                self._relevant_deductions[key].add(deduction)
     
     def seen_relevant_information(self):
         '''Has the applier been shown anything at all relevant?'''
         if not self._factfilters and not self._deductionfilters:
             return True # Otherwise this would always be false, and no filters means everything is good.
-        return (any(self._relevant_facts) or any(self._relevant_deductions))
+        return any(self._relevant_facts.values()) or any(self._relevant_deductions.values())
     
     def can_deduce(self):
         '''Does this applier have enough information to deduce anything?'''
         if self._custom_deduction_check is not None:
             return self._custom_deduction_check(self._relevant_facts, self._relevant_deductions)
-        return (all(facts for facts in self._relevant_facts) and
-                all(deductions for deductions in self._relevant_deductions))
+        return all(self._relevant_facts.values()) and all(self._relevant_deductions.values())
     
     def apply(self, add, allfacts, alldeductions):
         '''Apply the rule using the rule's own apply function.'''
         if self.can_deduce():
-            self._apply(self, add, allfacts, alldeductions, *self._relevant_facts.append(self._relevant_deductions))
+            self._apply(self, add, allfacts, alldeductions,
+                        **dict(self._relevant_facts, **self._relevant_deductions))
